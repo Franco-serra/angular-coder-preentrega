9e57456e4e342296b1c0d5a1521bda6574613eb2
Franco-serra <frankmourice28@gmail.com>
2025-03-17 22:39:46 -0300

    segundo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './pages/navbar/navbar.component';
+import { SidebarComponent } from './pages/sidebar/sidebar.component';
+import { MainContentComponent } from './pages/main-content/main-content.component';
+import { FooterComponent } from './pages/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SidebarComponent,
+    MainContentComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
